8eccd95049f7f73879817bb7ac98cc52fcee8a12
Aleks <achudars@gmail.com>
2013-01-08 11:42:26 +0000

    Problem 005 added

---
+++ PE005/src/PE005.java
@@ -0,0 +1,23 @@
+public class PE005 {
+	public static void main(String[] args) {
+		// There's no need to start from a smaller number, because we will be traversing from 20 to 11
+		// Numbers from 11 to 1 are excluded as they are multiples of the higher numbers
+		for (int i = 10; i <= 1000000000; i++) {
+			if (i % 20 == 0 && 
+				i % 19 == 0 && 
+				i % 18 == 0 && 
+				i % 17 == 0 && 
+				i % 16 == 0 && 
+				i % 15 == 0 && 
+				i % 14 == 0 && 
+				i % 13 == 0 && 
+				i % 12 == 0 && 
+				i % 11 == 0)
+			{
+				System.out.println(i);
+				break;
+			};
+		};
+		
+	}
+}

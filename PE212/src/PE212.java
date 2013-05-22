f91805be0afbf9d6dc86e3051d8317fe211be093
Aleks <achudars@gmail.com>
2013-05-22 08:43:54 +0100

    Problem 212 added
    Combined Volume of Cuboids

---
+++ PE212/src/PE212.java
@@ -0,0 +1,16 @@
+
+public class PE212 {
+
+	
+	public static void main(String[] args) {
+		long startTime = System.nanoTime();
+
+		/* no solution */
+
+		long endTime = System.nanoTime();
+		System.out.printf("Total Time: %.6f seconds\n",
+				((endTime - startTime) / 1000000000.0));
+
+	}
+
+}
